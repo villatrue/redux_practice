0b46388380808908369b18619e924acf7f4974ef
villatrue <scarter0313@gmail.com>
2019-11-20 19:49:09 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import allReducer from './reducers'
+// import { Provider } from "react-redux";
+
+const store = createStore(allReducer);
+
+    store.subscribe(()=> console.log(store.getState()));
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

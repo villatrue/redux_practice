5e669ee459697e4fac9c0e2db421da504fe8ff57
villatrue <scarter0313@gmail.com>
2019-11-20 20:42:50 -0500

    error with store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux'
+import {increment} from './actions/index'
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter{counter}</h1>
+      <button onClick={()=>dispatch(increment())}>+</button>
+      <button>-</button>
+      {isLogged? <h3>Info you can only see logged in</h3> : <h3>LoggIN</h3>}
     </div>
   );
 }
